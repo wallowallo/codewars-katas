7ed769aa60c5621b66d6a50ab6f6310f62f48593
Krister <krister.torsvik@gmail.com>
2016-11-01 16:57:45 +0100

    returns two decimals on a multi decimal number

---
+++ only-two-decimals.js
@@ -0,0 +1,23 @@
+// this function converts every number with decimals over 2. for example "12.11434223" to 12.11.
+// this function does not round up.
+function twoDecimalPlaces(n) {
+  let numberString = n.toString();
+  let atDot;
+  console.log('original number', numberString);
+  for (var i = 1; i <= numberString.length; i++) {
+    if(numberString.charAt(i) === '.') {
+      atDot = numberString.charAt(i);
+      let index = numberString.indexOf(atDot);
+      let removeDecimals = index + 3 - numberString.length;
+      if (removeDecimals === 0) {
+        return n;
+      }
+      return console.log(parseInt(numberString) + parseFloat(numberString.slice(index, removeDecimals)));
+    }
+  }
+}
+twoDecimalPlaces(12353434.45678);
+twoDecimalPlaces(12335.34);
+twoDecimalPlaces(123.59678678);
+twoDecimalPlaces(13.796);
+twoDecimalPlaces(1.784234);
