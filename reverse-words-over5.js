2b8cab532292d401560a6dd4664da200475609dd
Krister <krister.torsvik@gmail.com>
2016-11-02 16:42:07 +0100

    added best practice from other code warriors

--- reverse-words-over5.js
+++ reverse-words-over5.js
@@ -17,3 +17,10 @@ function spinWords(s){
 spinWords("hello there mate");
 spinWords("helloa");
 spinWords("hey fancy to meet you here");
+
+// other code warriors bestpractice
+function spinWords(words){
+  return words.split(' ').map(function (word) {
+    return (word.length > 4) ? word.split('').reverse().join('') : word;
+  }).join(' ');
+}
