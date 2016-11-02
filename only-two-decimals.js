ed99b4eaf089ad50a885b7d00466bc1f356c75ef
Krister <krister.torsvik@gmail.com>
2016-11-02 11:09:11 +0100

    reduce to 2 decimals

--- only-two-decimals.js
+++ only-two-decimals.js
@@ -1,3 +1,4 @@
+// this was my first attempt on the round up kata, that i misunderstood. Thought it was to reduce to 2 decimals.
 // this function converts every number with decimals over 2. for example "12.11434223" to 12.11.
 // this function does not round up.
 function twoDecimalPlaces(n) {
