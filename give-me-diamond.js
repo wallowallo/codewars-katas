db6c489471110ea3d59b07f74896187d583519ec
Krister <krister.torsvik@gmail.com>
2016-11-14 14:20:39 +0100

    wip: need to center

--- give-me-diamond.js
+++ give-me-diamond.js
@@ -24,10 +24,13 @@ function diamond(n){
   if(n % 2 === 0 || Math.sign(n) === -1) {
     return null;
   }
-  let diamonds = '*';
+  let array = []
+  const diamonds = '******************************************************************************';
   do {
-    console.log(n);
+    array.push(diamonds.split('', n).join(''));
     n -= 2;
-  } while(n > 0)
+  } while(n > 0);
+  return console.log(array.reverse().join('\n') + '\n' + array.reverse().splice(1,10).join('\n'));
 }
 diamond(5);
+diamond(7);
