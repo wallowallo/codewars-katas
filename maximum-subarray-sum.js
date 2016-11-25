3df28c2004ea62cd3a00bacc275efdf4eb9c150f
Krister <krister.torsvik@gmail.com>
2016-11-25 21:42:13 +0100

    wip: if array is negative and add togetehr contiguous subarrays

--- maximum-subarray-sum.js
+++ maximum-subarray-sum.js
@@ -1,4 +1,10 @@
-const  maxSequence = (arr) => {
-  arr.subarray();
+function maxSequence(arr) {
+  if(arr.length === 0 || arr === ) {
+    return 0; //should return null if array is empty or only negative variables
+  }
+  return console.log(arr.map( function(a, b) {return a+b}));
 }
 maxSequence([1, 2, 3, 4, -5, 6, 7])
+maxSequence([]);
+maxSequence([-2]);
+
