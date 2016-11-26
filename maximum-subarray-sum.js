d981abad36601283d74c97a8fbb541d8365161ea
Krister <krister.torsvik@gmail.com>
2016-11-26 22:53:34 +0100

    wip: codewar kata that uses a max sequence

--- maximum-subarray-sum.js
+++ maximum-subarray-sum.js
@@ -1,8 +1,9 @@
 function maxSequence(arr) {
-  if(arr.length === 0 || arr === ) {
+  if(arr.length === 0 || arr[i] < 0 ) {
     return 0; //should return null if array is empty or only negative variables
   }
   return console.log(arr.map( function(a, b) {return a+b}));
+  //needs other logic to add them together.
 }
 maxSequence([1, 2, 3, 4, -5, 6, 7])
 maxSequence([]);
