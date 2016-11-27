1457481db563c992f1c3e1b1b94a2fd7ecb194d9
Krister <krister.torsvik@gmail.com>
2016-11-27 21:33:29 +0100

    used reduce to add them together

--- maximum-subarray-sum.js
+++ maximum-subarray-sum.js
@@ -1,8 +1,8 @@
 function maxSequence(arr) {
-  if(arr.length === 0 || arr[i] < 0 ) {
+  if(arr.length === 0) {
     return 0; //should return null if array is empty or only negative variables
   }
-  return console.log(arr.map( function(a, b) {return a+b}));
+  return console.log(arr.reduce((a, b) =>  a+b, 0));
   //needs other logic to add them together.
 }
 maxSequence([1, 2, 3, 4, -5, 6, 7])
