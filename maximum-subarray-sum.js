65b189a4c34ef10347b5ae818336aee5cbb2f090
Krister <krister.torsvik@gmail.com>
2016-11-28 18:14:56 +0100

    working on the max subarray logic

--- maximum-subarray-sum.js
+++ maximum-subarray-sum.js
@@ -1,10 +1,13 @@
 function maxSequence(arr) {
   let checkArray = [];
   let map = arr.map((a, b) => a < 0 ? checkArray.push(a) : 0)
+  let maxCallback = (max, cur) => Math.max(max, cur);
   if(arr.length === 0 || arr.length == checkArray.length) { 
     return console.log('zero', 0);
   }
-  return console.log(arr.reduce((a, b) =>  a+b, 0));
+  return console.log(arr.map().reduce(maxCallback);
+  //return console.log(arr.reduce((a, b) =>  a+b, 0));
+  //use Math.max(...Array)
   //needs other logic to add them together.
 }
 maxSequence([1, 2, 3, 4, -5, 6, 7])
