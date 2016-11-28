4959b83c3eeee7b859fc118ef2f2cbc142f97e0d
Krister <krister.torsvik@gmail.com>
2016-11-28 10:08:08 +0100

    returns 0 if whole array is negative

--- maximum-subarray-sum.js
+++ maximum-subarray-sum.js
@@ -1,11 +1,13 @@
 function maxSequence(arr) {
-  if(arr.length === 0) {
-    return 0; //should return null if array is empty or only negative variables
+  let checkArray = [];
+  let map = arr.map(a => a < 0 ? checkArray.push(a) : 0)
+  if(arr.length === 0 || arr.length == checkArray.length) { 
+    return console.log('zero', 0); //should return null if array is empty or only negative variables
   }
   return console.log(arr.reduce((a, b) =>  a+b, 0));
   //needs other logic to add them together.
 }
 maxSequence([1, 2, 3, 4, -5, 6, 7])
 maxSequence([]);
-maxSequence([-2]);
+maxSequence([-2, -1, -3]);
 
