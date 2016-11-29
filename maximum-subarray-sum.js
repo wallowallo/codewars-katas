2749a0792870712c25e4dde18f7bbf0306c0c2cc
Krister <krister.torsvik@gmail.com>
2016-11-29 20:36:24 +0100

    made the code a bit easier to read so far

--- maximum-subarray-sum.js
+++ maximum-subarray-sum.js
@@ -1,18 +1,19 @@
 function maxSequence(arr) {
-  let checkArray = [];
-  let map = arr.map((a, b) => a < 0 ? checkArray.push(a) : 0)
-  let maxCallback = (max, cur) => Math.max(max, cur);
-  if(arr.length === 0 || arr.length == checkArray.length) { 
+  let newArray = [];
+  let map = arr.map((a, b) => a < 0 ? newArray.push(a) : 0)
+  if(arr.length === 0 || arr.length == newArray.length) { 
     return 0;
   }
-  let newArray = [];
-  arr.forEach((element,index) => newArray.push(arr.slice(index).reduce((a,b) => a+b)));
-  return newArray.reduce(maxCallback);
-  //return console.log(arr.map().reduce(maxCallback);
+  newArray = [];
+  arr.forEach((element,index) => {
+    newArray.push(arr.slice(index)
+    .reduce((a,b) => a+b));
+  });
+  return console.log(newArray.reduce((max, cur) => Math.max(max,cur)));
   //iterate over the array and slice it up into multiple sub arrays, add them together. Push them to an array then use math.max to decide the greatest sub array value.    
-  //use Math.max(...Array)
 }
 maxSequence([1, 2, 3, 4, -5, 6, 7])
 maxSequence([]);
 maxSequence([-2, -1, -3]);
+maxSequence([9, 10, 3, 4, -5, 6, 7])
 
