69c2c4161f16dacbfc3a2b8748c7a8c552359c59
Krister <krister.torsvik@gmail.com>
2016-11-29 12:39:20 +0100

    need slice to iterate from middle and end off array and then call maxCallback

--- maximum-subarray-sum.js
+++ maximum-subarray-sum.js
@@ -5,7 +5,10 @@ function maxSequence(arr) {
   if(arr.length === 0 || arr.length == checkArray.length) { 
     return 0;
   }
-  return console.log(arr.map().reduce(maxCallback);
+  let newArray = [];
+  arr.forEach((element,index) => newArray.push(arr.slice(index).reduce((a,b) => a+b)));
+  return newArray.reduce(maxCallback);
+  //return console.log(arr.map().reduce(maxCallback);
   //iterate over the array and slice it up into multiple sub arrays, add them together. Push them to an array then use math.max to decide the greatest sub array value.    
   //use Math.max(...Array)
 }
