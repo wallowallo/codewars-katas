dae53a83263b5f4d5004fa7ffb43acfb034bf298
Krister <krister.torsvik@gmail.com>
2016-12-07 14:30:59 +0100

    created story to the task

---
+++ broken-sequence.js
@@ -0,0 +1,4 @@
+function findMissingNumber(sequence){
+  //use split() to create an array where you can go over with map() on the array to match it with the index
+  // if the index is not equal to the array then return that value or replace the value with the index
+}
