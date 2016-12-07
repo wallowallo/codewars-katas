c7fff872090a85a56d42af1c4b7a1a625fe7b637
Krister <krister.torsvik@gmail.com>
2016-12-07 12:26:10 +0100

    added best practice

--- find-number-in-array.js
+++ find-number-in-array.js
@@ -3,5 +3,7 @@ function stray(n) {
   n.map((e, i) => (e !== n[i+1] && e !== n[i-1]) ? newArray.push(e):0);
   return newArray[0];
 }
-
 stray([17,17,17,17,3]);
+
+//other codewarriors best practice:
+const stray = nums => nums.reduce((a, b) => a ^ b);
