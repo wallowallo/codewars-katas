fe3021197fb7859302ce39576ae392bc807478e4
Krister <krister.torsvik@gmail.com>
2016-12-07 21:41:35 +0100

    final answer

--- validate-pincode.js
+++ validate-pincode.js
@@ -1,13 +1,12 @@
 function validatePIN (pin) {
   let array = pin.split('');
-  array.map((e, i) => isNaN(e) ? array.splice(i, 1) : e);
-  console.log(array);
+  array.map((e, i) => isNaN(e) ? array.splice(e) : e);
   if(array.length === 4 || array.length === 6) {
-    return console.log('true', pin);
+    return true;
   }
-  return console.log('false',pin);
+  return false;
 }
-validatePIN('1234');
+validatePIN('-1234');
 validatePIN('123aa');
 validatePIN('ab2345');
 validatePIN('123456')
