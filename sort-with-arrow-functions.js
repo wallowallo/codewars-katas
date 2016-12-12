5960bbf6c22647be1f313681f4fcea568079d674
Krister <krister.torsvik@gmail.com>
2016-12-12 21:56:43 +0100

    testing sort function

--- sort-with-arrow-functions.js
+++ sort-with-arrow-functions.js
@@ -1,3 +1,11 @@
 var OrderPeople = function(people){
-  return people.sort( => ); //complete this function
+  return people.sort((a,b) => {
+    if(a.age > b.age) {
+      return 1;
+    }
+    else if(a.age < b.age) {
+      return -1;
+    }
+    return 0;
+  });
 }
