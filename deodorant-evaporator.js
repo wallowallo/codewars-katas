0d191addc8f5ba451a38ebad4b076f35a66af013
Krister <krister.torsvik@gmail.com>
2016-12-12 23:16:42 +0100

    needs percent logic inside the for loop

--- deodorant-evaporator.js
+++ deodorant-evaporator.js
@@ -1,8 +1,15 @@
 function evaporator(content, evap_per_day, threshold){ 
- let days = 0;
- let renderedUseless = content / threshold;
- console.log(renderedUseless);
- //create for loop to increase days and divide the content with the evap_per_day until it hits the threshold
+  let days = 0;
+  let renderedUseless = content / threshold;
+ //create while loop to increase days and divide the content with the evap_per_day until it hits the threshold
+  while(content > renderedUseless) {
+    let subtract = 1;
+    content -= subtract;
+    days++;
+    console.log(content);
+  }
+ console.log('content', content);
+ console.log('days', days);
 }
 evaporator(10,10,10);
 //should be 22
