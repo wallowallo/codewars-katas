5f73b4463039f2d62c84fc47f6b552eb66695646
Krister <krister.torsvik@gmail.com>
2016-12-12 22:36:04 +0100

    added story and renderedUseless variable

--- deodorant-evaporator.js
+++ deodorant-evaporator.js
@@ -1,3 +1,8 @@
 function evaporator(content, evap_per_day, threshold){ 
-  return -1;
+ let days = 0;
+ let renderedUseless = content / threshold;
+ console.log(renderedUseless);
+ //create for loop to increase days and divide the content with the evap_per_day until it hits the threshold
 }
+evaporator(10,10,10);
+//should be 22
