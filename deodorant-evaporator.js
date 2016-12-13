1f9350d485bc48bd558fc93796f3aa5826f026e4
Krister <krister.torsvik@gmail.com>
2016-12-13 22:36:35 +0100

    final answer

--- deodorant-evaporator.js
+++ deodorant-evaporator.js
@@ -1,15 +1,16 @@
 function evaporator(content, evap_per_day, threshold){ 
   let days = 0;
-  let renderedUseless = content / threshold;
+  let renderedUseless =  threshold/100 * content;
  //create while loop to increase days and divide the content with the evap_per_day until it hits the threshold
   while(content > renderedUseless) {
-    let subtract = content / evap_per_day;
+    let subtract = evap_per_day/100 * content;
     content -= subtract;
     days++;
-    console.log(content);
   }
  console.log('content', content);
  console.log('days', days);
 }
 evaporator(10,10,10);
 //should be 22
+evaporator(10,10,5);
+//should be 29
