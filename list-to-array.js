a8b52f858600c11cb991f15307c4b9240445aa61
Krister <krister.torsvik@gmail.com>
2016-12-16 22:03:13 +0100

    added story

--- list-to-array.js
+++ list-to-array.js
@@ -1,3 +1,4 @@
 function listToArray(list) {
-  //write your code here
+  //find out how to look into objects within objects
+  //target the value that is relevant and push it into an array/ use slice to make a new array with those values
 }
