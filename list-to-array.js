b10aeb252e2a9d1076c0fad532197ef9b8f0000b
Krister <krister.torsvik@gmail.com>
2016-12-19 13:01:47 +0100

    find a way to loop through the next and get the value if its not null

--- list-to-array.js
+++ list-to-array.js
@@ -3,4 +3,9 @@ function listToArray(list) {
   //target the value that is relevant and push it into an array/ use slice to make a new array with those values
   let array = [];
   array.push(list.value);
+  if(list.next !== null) {
+    array.push(list.next.value);
+    array.push(list.next.next.value);
+  }
+  return array;
 }
