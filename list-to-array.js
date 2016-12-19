e05ac55f9a3250a7f523bf33de43cb821383aef2
Krister <krister.torsvik@gmail.com>
2016-12-19 12:34:34 +0100

    need to loop through the other next objects and get their values

--- list-to-array.js
+++ list-to-array.js
@@ -1,4 +1,6 @@
 function listToArray(list) {
   //find out how to look into objects within objects
   //target the value that is relevant and push it into an array/ use slice to make a new array with those values
+  let array = [];
+  array.push(list.value);
 }
