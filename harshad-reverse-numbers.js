bf7f677b3702e979255c2801759d698feaf4d1b5
Krister <krister.torsvik@gmail.com>
2016-12-20 22:58:26 +0100

    found out if the number is harshad or not

--- harshad-reverse-numbers.js
+++ harshad-reverse-numbers.js
@@ -2,10 +2,16 @@ function numberJoy(n) {
   // find out if the number is a harshad number, then find out if that number is multiplied by the number reversed
   //split the number into an array and add the numbers together to find out if the added number divides the input to an integer
   //if that integer is the added number reversed then let it equal to true else set it to false
-  let array = (''+n).split('').map(a => parseInt(a)).reduce((a,b) => a+b);
-  console.log(array); 
+  let addInput = (''+n).split('').map(a => parseInt(a)).reduce((a,b) => a+b);
+  let harshadNumber = n / addInput;
+  console.log(harshadNumber);
+  if(harshadNumber === parseInt(harshadNumber, 10)) {
+    return console.log('true');
+  }
+  return console.log('false');
 }
 numberJoy(1729);
 //should be 1+7+2+9 = 19
 //then 19 x 91 to multiply with the reversed number should equal to input
 //this answer should return true
+numberJoy(213);
