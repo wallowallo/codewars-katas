bb317da31b12c1a62e5086315a08be9722f1d855
Krister <krister.torsvik@gmail.com>
2016-12-21 11:17:53 +0100

    removed unwanted lines

--- harshad-reverse-numbers.js
+++ harshad-reverse-numbers.js
@@ -8,13 +8,6 @@ function numberJoy(n) {
   return false;
 }
 numberJoy(1729);
-//should be 1+7+2+9 = 19
-//then 19 x 91 to multiply with the reversed number should equal to input
-//this answer should return true
-numberJoy(213);
-numberJoy(1);
-numberJoy(81);
-numberJoy(1458);
 
 //best practice/clever from other codewarriors
 function numberJoy(n) {
